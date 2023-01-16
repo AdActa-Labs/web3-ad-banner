cf503fcddc51a405270c7583d11750a7436e06de
Michael Wang <michaelwang11394@gmail.com>
2023-01-15 18:33:49 -0800

    Add Mint NFT Contract

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  // Do i need to add path here?
+};
+
+export default config;

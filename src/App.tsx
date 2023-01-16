06c0cb135efe48a2d96caac798c7d7f31f5e744d
Kenta Onimura <kentaonex@gmail.com>
2023-01-16 18:05:56 -0500

    Add publisher referral and remove NFT count

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./app.css";
-import Banner from "./components/banner.tsx";
+import "./App.css";
+import Banner from "./components/banner";
 
 const App: React.FC = () => {
   return (
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -4,10 +4,19 @@ interface Props {
   message: string;
 }
 
+const LOCAL_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 const Banner: React.FC<Props> = ({ message }) => {
+  const mintToken = async () => {};
+
   return (
     <div className="banner">
-      <button>Hi</button>
+      <input
+        type="text"
+        name="referral"
+        placeholder="Referral Reward Destination"
+      />
+      <button onClick={mintToken}>Mint NFT</button>
     </div>
   );
 };
